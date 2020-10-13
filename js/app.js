8411e2d12b5ced2bbf97d8bc0d96b3b11dbbe0ea
Gabriela Yan <gabriela.maldonado.yan@gmail.com>
2020-10-13 17:05:36 -0500

    Query to DB for get Clients

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@
     let DB;
 
     document.addEventListener('DOMContentLoaded', () => {
-        createDB()
+        createDB();
+
+        if(window.indexedDB.open('crm',1)){
+            getClients();
+        }
     });
 
     //Create the DB of IndexDB
@@ -33,5 +37,52 @@
 
         };
 
+    }
+
+    function getClients() {
+        const openConnection = window.indexedDB.open('crm',1);
+
+        openConnection.onerror = function() {
+            console.log('Ocurrió un error');    
+        };
+
+        openConnection.onsuccess = function() {
+            DB = openConnection.result;
+
+            const objectStore = DB.transaction('crm').objectStore('crm');
+
+            objectStore.openCursor().onsuccess = function(e){
+                const cursor = e.target.result;
+
+                if(cursor){
+                    // console.log(cursor.value); Test
+                    const {name, email, phone, company, id} = cursor.value;
+
+                    const customersList = document.querySelector('#listado-clientes');
+
+                    customersList.innerHTML += ` <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${name} </p>
+                                <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                                <p class="text-gray-700">${phone}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                                <p class="text-gray-600">${company}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                                <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                            </td>
+                        </tr>
+                    `;
+
+                    cursor.continue();
+                } else {
+                    console.log('No ha más registros');
+                }
+            }
+        }
     }
 })();
